33bafe574588c3f7a8a22fcacea9703c303d07c6
Jhon Perez <jhonperez.j95@gmail.com>
2020-08-22 23:50:21 -0500

    encrypt password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,13 +1,44 @@
 const { response, request } = require('express');
+const bcrypt = require('bcryptjs');
 
-const createUser = (req, res) => {
-  const { body } = req;
+const User = require('../models/User');
 
-  res.json({
-    ok: true,
-    msg: 'Creando usuario',
-    data: body,
-  });
+const createUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // Validator the email
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: `El email ${email} ya existe`,
+      });
+    }
+    user = new User(req.body);
+
+    // Encrypt password
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
+
+    // Save user in db
+    const { _id, name } = await user.save();
+
+    res.status(201).json({
+      ok: true,
+      msg: 'Usuario creado',
+      user: {
+        _id,
+        name,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Error al  crear el usuario',
+      error: error,
+    });
+  }
 };
 
 const renovateToken = (req = request, res = response) => {
