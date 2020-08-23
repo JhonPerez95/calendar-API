74654f7f7ab73628abfdeb72550196c6ec358e5c
Jhon Perez <jhonperez.j95@gmail.com>
2020-08-22 22:38:22 -0500

    add database

---
+++ src/database/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('DB online');
+  } catch (error) {
+    throw new Error('Error al conectar a la base de datos');
+  }
+};
+
+module.exports = {
+  connectDb,
+};
--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const { connectDb } = require('./database/database');
 
 // Initializations
 const app = express();
 
+// Database
+connectDb();
+
 // Directorio public
 app.use(express.static('public'));
 
-//
+// Config
 app.use(express.json());
 
 // Routes
