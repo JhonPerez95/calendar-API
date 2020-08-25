b382f0b4d525ec7915bbffe87237803c73cc487e
Jhon Perez <jhonperez.j95@gmail.com>
2020-08-24 21:59:51 -0500

    update code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 
 const app = require('./server');
-const port = process.env.PORT || 3030;
 
-app.listen(port, () => {
-  console.log(`Server started on ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on ${process.env.PORT}`);
 });
