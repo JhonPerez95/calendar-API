092f0d99b504f4abbf5f45aff318e6c5ef4aea09
Jhon Perez <jhonperez.j95@gmail.com>
2020-09-02 21:42:05 -0500

    calendar app

---
+++ public/precache-manifest.05f2b0288e476d131cb9a07152f3fc95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7037885b0f30cd08d9db95b4d577a29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce8a1fcb0b5481aab0e2",
+    "url": "/static/css/2.dbd11c43.chunk.css"
+  },
+  {
+    "revision": "eeab93fed9b58b97d885",
+    "url": "/static/css/main.bb6b0fdb.chunk.css"
+  },
+  {
+    "revision": "ce8a1fcb0b5481aab0e2",
+    "url": "/static/js/2.25b26877.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.25b26877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeab93fed9b58b97d885",
+    "url": "/static/js/main.adc331df.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
